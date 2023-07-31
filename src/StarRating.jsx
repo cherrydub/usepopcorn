666a518d84d99810f6b7a72a77c190b812d9acdf
cherrydub <cherrydub@gmail.com>
2023-07-31 19:09:44 +0100

    improved reusability of props

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -12,12 +12,13 @@ const starContainerStyle = {
 
 
 
-export default function StarRating({maxRating = 5, color = '#fcc419', size = 48, className, messages=[], defaultRating = 0}) {
+export default function StarRating({maxRating = 5, color = '#fcc419', size = 48, className, messages=[], defaultRating = 0, onSetRating}) {
     const [rating, setRating] = useState(defaultRating)
     const [tempRating, setTempRating] = useState(0)
 
     function handleRating(rating){
         setRating(rating)
+        onSetRating(rating)
     }
 
     const textStyle = {
