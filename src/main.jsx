29ede6371c045735ea634abc39ee28848b2ed3b0
cherrydub <cherrydub@gmail.com>
2023-07-31 18:43:18 +0100

    adding star hover effects

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5}/>
-    <StarRating maxRating={10}/>
+
   </React.StrictMode>,
 )
